9591fe183cc99ed986777f45e6060ae79e885849
Michał Szewczak <michal@szewczak.email>
2020-03-28 17:51:30 +0000

    add HTTPoison

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule CovidHotspot.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.6"}
     ]
   end
 end
