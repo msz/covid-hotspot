7feca4bd775985208e85c68e0ff1cc37c0df8f40
Michał Szewczak <michal@szewczak.email>
2020-03-28 15:42:42 +0000

    collate all areas

--- lib/covid_hotspot.ex
+++ lib/covid_hotspot.ex
@@ -3,39 +3,35 @@ defmodule CovidHotspot do
 
   alias CovidHotspot.TestCovidData
   alias CovidHotspot.PopulationData
-
-  @covid_id_to_population_id %{
-    "unitedstates" => "USA",
-    "chinamainland" => "CHN",
-    "italy" => "ITA"
-  }
+  alias CovidHotspot.Collate
 
   def generate_covid_hotspot_data do
     get_covid_data()
     |> Map.get("areas")
     |> Enum.reject(fn area ->
-      covid_id_to_population_id(area["id"]) == nil
+      Collate.covid_id_to_population_id(area["id"]) == nil
     end)
-    |> Enum.map(fn area ->
-      %{
-        "id" => id,
-        "displayName" => display_name,
-        "totalConfirmed" => total_confirmed,
-        "totalDeaths" => total_deaths,
-        "totalRecovered" => total_recovered
-      } = area
+    |> Enum.map(fn
+      area ->
+        %{
+          "id" => id,
+          "displayName" => display_name,
+          "totalConfirmed" => total_confirmed,
+          "totalDeaths" => total_deaths,
+          "totalRecovered" => total_recovered
+        } = normalize_covid_area(area)
 
-      active_cases = total_confirmed - total_deaths - total_recovered
-      population = get_population(id)
-      fraction = Float.round(active_cases / population, 5)
+        active_cases = total_confirmed - total_deaths - total_recovered
+        population = get_population(id)
+        fraction = Float.round(active_cases / population, 5)
 
-      %{
-        id: id,
-        display_name: display_name,
-        active_cases: active_cases,
-        population: population,
-        fraction: fraction
-      }
+        %{
+          id: id,
+          display_name: display_name,
+          active_cases: active_cases,
+          population: population,
+          fraction: fraction
+        }
     end)
     |> Enum.reject(fn %{fraction: fraction} -> fraction == 0.0 end)
     |> Enum.sort(fn %{fraction: fraction1}, %{fraction: fraction2} ->
@@ -43,9 +39,20 @@ defmodule CovidHotspot do
     end)
   end
 
-  defp get_population(covid_id) do
-    population_id = covid_id_to_population_id(covid_id)
+  defp normalize_covid_area(%{"totalConfirmed" => total} = area) when is_nil(total),
+    do: normalize_covid_area(%{area | "totalConfirmed" => 0})
 
+  defp normalize_covid_area(%{"totalDeaths" => total} = area) when is_nil(total),
+    do: normalize_covid_area(%{area | "totalDeaths" => 0})
+
+  defp normalize_covid_area(%{"totalRecovered" => total} = area) when is_nil(total),
+    do: normalize_covid_area(%{area | "totalRecovered" => 0})
+
+  defp normalize_covid_area(%{} = area), do: area
+
+  defp get_population(covid_id) do
+    IO.inspect(covid_id)
+    population_id = Collate.covid_id_to_population_id(covid_id)
     # TODO make this more efficient by making PopulationData a dictionary
     %{"population" => population} =
       Enum.find(PopulationData.get_data(), fn %{"country_code" => country_code} ->
@@ -53,10 +60,6 @@ defmodule CovidHotspot do
       end)
 
     population
-  end
-
-  defp covid_id_to_population_id(covid_id) do
-    @covid_id_to_population_id[covid_id]
   end
 
   defp get_covid_data do
