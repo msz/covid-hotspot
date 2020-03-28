7ffced46112c8bd9be3a40f3ed0f452787440728
Michał Szewczak <michal@szewczak.email>
2020-03-28 16:03:05 +0000

    population as million fraction

--- lib/covid_hotspot_web/views/page_view.ex
+++ lib/covid_hotspot_web/views/page_view.ex
@@ -1,6 +1,11 @@
 defmodule CovidHotspotWeb.PageView do
   use CovidHotspotWeb, :view
 
+  def million_fraction(number) do
+    fraction = (number / 1_000_000)
+    Float.round(fraction, 2)
+  end
+
   def percent(fraction) do
     Float.round(fraction * 100, 5)
   end
